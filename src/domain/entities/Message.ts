5303449a548df996a3f3be1a63d447f92c8a46fc
RzayevTamerlan <tamerlan.rzev@gmail.com>
2025-09-02 15:10:56 +0400

    bugfix: Fix import error in message.ts

--- src/domain/entities/Message.ts
+++ src/domain/entities/Message.ts
@@ -2,7 +2,7 @@ import { BaseEntity } from '@domain/entities/BaseEntity.ts';
 import { Chat } from '@domain/entities/Chat.ts';
 import { Media } from '@domain/entities/Media.ts';
 import { User } from '@domain/entities/User.ts';
-import { MessageType } from '@domain/enums/MediaType.ts';
+import { MessageType } from '@domain/enums/MessageType.ts';
 
 export type Message = BaseEntity & {
   type: MessageType;
